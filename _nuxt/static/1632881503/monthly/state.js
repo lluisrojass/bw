2af76da968a599c7d418c31da9b339a202ed088c
lluisrojass <lluisrojass@users.noreply.github.com>
2021-09-29 02:11:47 +0000

    deploy: e7c8a2c930febc1d8eb0e303ad98004bfd9f8f19

---
+++ _nuxt/static/1632881503/monthly/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fbw\u002F_nuxt\u002Fstatic\u002F1632881503",layout:"default",error:a,serverRendered:true,routePath:"\u002Fmonthly",config:{_app:{basePath:"\u002Fbw\u002F",assetsPath:"\u002Fbw\u002F_nuxt\u002F",cdnURL:a}}}}(null));
