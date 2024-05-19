52c6a7c0f464d6e4e4c86aff2adf1571b925b076
lluisrojass <lluisrojass@users.noreply.github.com>
2024-05-19 01:59:46 +0000

    deploy: d5639bde2266ecbf45b8da659569c05eae2db43b

---
+++ _nuxt/static/1716083983/image/ea1418554126/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fphotos\u002F_nuxt\u002Fstatic\u002F1716083983",layout:"default",error:a,serverRendered:true,routePath:"\u002Fimage\u002Fea1418554126",config:{_app:{basePath:"\u002Fphotos\u002F",assetsPath:"\u002Fphotos\u002F_nuxt\u002F",cdnURL:a}}}}(null));
